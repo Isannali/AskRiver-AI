5780defe90657091f72aaa1b0cd75a0022bb2390
Isannali <aliandiihsan59@gmail.com>
2026-08-31 07:18:30 +0700

    change main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,10 @@
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import "pages/chat_page.dart";
 import "providers/chat_provider.dart";
 import "theme/app_theme.dart";
 
-Future <void>  main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(
     MultiProvider(
       providers: [
